ab56c5a8d7040a00f93b6de4bfeb90b66593e3c3
lucemia <lucemia@gmail.com>
2015-11-05 14:03:59 +0800

    add demo

--- mysite/mysite/static/js/demo.js
+++ mysite/mysite/static/js/demo.js
@@ -17,7 +17,7 @@ function createChart() {
         }],
         categoryAxis: {
             categories: [
-                "Team work", "Coding", "Testing", "Academic", "Innovation", "Community", "Project Management",
+            "Team work", "Coding", "Testing", "Academic", "Innovation", "Community", "Project Management",
             ],
             majorGridLines: {
                 visible: false
@@ -48,214 +48,214 @@ function sliderOnChange1(e) {
     }
 }
 function sliderOnChange2(e) {
-console.log("Change :: new value is: " + e.value);
-var chart = $("#chart").data("kendoChart");
-chart.options.series[0].data[1] = e.value;
-chart.redraw();
-if (e.value>7){
-$("#coding").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    var chart = $("#chart").data("kendoChart");
+    chart.options.series[0].data[1] = e.value;
+    chart.redraw();
+    if (e.value>7){
+        $("#coding").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 
 }
 function sliderOnChange3(e) {
-console.log("Change :: new value is: " + e.value);
-var chart = $("#chart").data("kendoChart");
-chart.options.series[0].data[2] = e.value;
-chart.redraw();
-if (e.value>7){
-$("#acedemic").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    var chart = $("#chart").data("kendoChart");
+    chart.options.series[0].data[2] = e.value;
+    chart.redraw();
+    if (e.value>7){
+        $("#acedemic").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 
 }
 function sliderOnChange4(e) {
-console.log("Change :: new value is: " + e.value);
-var chart = $("#chart").data("kendoChart");
-chart.options.series[0].data[3] = e.value;
-chart.redraw();
-if (e.value>7){
-$("#testing").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    var chart = $("#chart").data("kendoChart");
+    chart.options.series[0].data[3] = e.value;
+    chart.redraw();
+    if (e.value>7){
+        $("#testing").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 
 }
 function sliderOnChange5(e) {
-console.log("Change :: new value is: " + e.value);
-var chart = $("#chart").data("kendoChart");
-chart.options.series[0].data[4] = e.value;
-chart.redraw();
-if (e.value>7){
-$("#innovation").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    var chart = $("#chart").data("kendoChart");
+    chart.options.series[0].data[4] = e.value;
+    chart.redraw();
+    if (e.value>7){
+        $("#innovation").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 
 }
 function sliderOnChange6(e) {
-console.log("Change :: new value is: " + e.value);
-var chart = $("#chart").data("kendoChart");
-chart.options.series[0].data[5] = e.value;
-chart.redraw();
-if (e.value>7){
-$("#community").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    var chart = $("#chart").data("kendoChart");
+    chart.options.series[0].data[5] = e.value;
+    chart.redraw();
+    if (e.value>7){
+        $("#community").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 
 }
 function sliderOnChange7(e) {
-console.log("Change :: new value is: " + e.value);
-var chart = $("#chart").data("kendoChart");
-chart.options.series[0].data[6] = e.value;
-chart.redraw();
-if (e.value>7){
-$("#pm").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    var chart = $("#chart").data("kendoChart");
+    chart.options.series[0].data[6] = e.value;
+    chart.redraw();
+    if (e.value>7){
+        $("#pm").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 
 }
 
 function sliderOnChange8(e) {
-console.log("Change :: new value is: " + e.value);
-if (e.value>7){
-$("#work").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    if (e.value>7){
+        $("#work").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 }
 function sliderOnChange9(e) {
-console.log("Change :: new value is: " + e.value);
-if (e.value>7){
-$("#edu").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    if (e.value>7){
+        $("#edu").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 }
 
 function sliderOnChange10(e) {
-console.log("Change :: new value is: " + e.value);
-if (e.value>7){
-$("#score").click();
-} else if (e.value <3){
-$("#all").click();
-}
+    console.log("Change :: new value is: " + e.value);
+    if (e.value>7){
+        $("#score").click();
+    } else if (e.value <3){
+        $("#all").click();
+    }
 }
 
 
 function sliderOnSlide(e) {
-console.log("Slide :: new slide value is: " + e.value);
+    console.log("Slide :: new slide value is: " + e.value);
 }
 
 
 $(document).ready(function() {
-$('#Container').mixItUp();
+    $('#Container').mixItUp();
 
-$("#slider1").kendoSlider({
-change: sliderOnChange1,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
-$("#slider2").kendoSlider({
-change: sliderOnChange2,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
-$("#slider3").kendoSlider({
-change: sliderOnChange3,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
-$("#slider4").kendoSlider({
-change: sliderOnChange4,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
-$("#slider5").kendoSlider({
-change: sliderOnChange5,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
-$("#slider6").kendoSlider({
-change: sliderOnChange6,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
-$("#slider7").kendoSlider({
-change: sliderOnChange7,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
-$("#slider8").kendoSlider({
-change: sliderOnChange8,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
-$("#slider9").kendoSlider({
-change: sliderOnChange9,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
+    $("#slider1").kendoSlider({
+        change: sliderOnChange1,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
+    $("#slider2").kendoSlider({
+        change: sliderOnChange2,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
+    $("#slider3").kendoSlider({
+        change: sliderOnChange3,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
+    $("#slider4").kendoSlider({
+        change: sliderOnChange4,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
+    $("#slider5").kendoSlider({
+        change: sliderOnChange5,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
+    $("#slider6").kendoSlider({
+        change: sliderOnChange6,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
+    $("#slider7").kendoSlider({
+        change: sliderOnChange7,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
+    $("#slider8").kendoSlider({
+        change: sliderOnChange8,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
+    $("#slider9").kendoSlider({
+        change: sliderOnChange9,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
 
-$("#slider10").kendoSlider({
-change: sliderOnChange10,
-slide: sliderOnSlide,
-min: 0,
-max: 10,
-smallStep: 1,
-largeStep: 10,
-value: 5,
-showButtons: false
-});
+    $("#slider10").kendoSlider({
+        change: sliderOnChange10,
+        slide: sliderOnSlide,
+        min: 0,
+        max: 10,
+        smallStep: 1,
+        largeStep: 10,
+        value: 5,
+        showButtons: false
+    });
 
 
 });
