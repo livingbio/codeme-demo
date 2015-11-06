fda1df1164521f0d142ac8c93503b3e7428b83dd
lucemia <lucemia@gmail.com>
2015-11-06 14:59:45 +0800

    backup work

--- mysite/mysite/mysite/urls.py
+++ mysite/mysite/mysite/urls.py
@@ -1,20 +1,21 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
 from django.contrib import admin
-from django_kss.views import AutoStyleGuideView
+from django.shortcuts import render
 import django_kss.urls
 
 admin.autodiscover()
 
 
+def demo_view(request):
+    return render(request, "base.html", {"resumes": []})
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
+# Examples:
+# url(r'^$', 'mysite.views.home', name='home'),
+# url(r'^mysite/', include('mysite.foo.urls')),
 
-    url(r'^$', TemplateView.as_view(template_name='base.html')), 
+    url(r'^$', demo_view),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^style_guide/', include(django_kss.urls)),
 )
